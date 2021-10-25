5cfce1343b57176cc14fb23db40a9ff59327287a
nick54785478 <nick54785478@gmail.com>
2021-10-25 16:04:20 +0800

    add gitignore

--- src/main/java/cf/cvb14795/Coupon/controller/AdminController.java
+++ src/main/java/cf/cvb14795/Coupon/controller/AdminController.java
@@ -22,11 +22,11 @@ import cf.cvb14795.member.dao.IMemberService;
 @Controller
 @RequestMapping("/Coupon")
 public class AdminController {
+
 	private final static String PREFIX = "Coupon/";
-	
 	IQuestionnaireService qService;
 	IMemberService mService;
-	
+
 	@Autowired
 	public AdminController(IQuestionnaireService qService, IMemberService mService) {
 		this.qService = qService;
@@ -46,9 +46,9 @@ public class AdminController {
 		} else if("U".equals(action)) {
 			List<QuestionnaireBean> sendUsersCoupons = qService.SendUsersCoupons();
 			model.addAttribute("sendUsersCoupons", sendUsersCoupons);
-			 
+			
 			/* =====由url的完整路徑拼出前綴路徑===== */
-            URL u;
+			URL u;
 			try {
 				u = new URL(request.getRequestURL().toString());
 			} catch (MalformedURLException e) {
@@ -58,33 +58,33 @@ public class AdminController {
 				System.out.println("*****將跳轉回首頁*****");
 				return "redirect:/Home";
 			}
-            StringBuilder sb = new StringBuilder();
-            // u.getProtocol(): "http" or "https"
-            sb.append(u.getProtocol()+"://");
-            // u.getAuthority(): localhost:8080
-            sb.append(u.getAuthority());
-            // request.getContextPath(): /FoodMap04
-            sb.append(request.getContextPath());
-            String baseUrl = sb.toString();
-            
-            /* =====寄信===== */
-            CouponUsageUtil couponUsage = new CouponUsageUtil(baseUrl);
-            System.out.println("================正在寄送Email...==================");
-            for (QuestionnaireBean b : sendUsersCoupons) {
-                System.out.println(b.getId());
-                /* ================================== */
-                /* 待修改成問卷表格與會員表格join的形式 */
-                
-                // 由問卷表格查詢該身分證對應的會員
-                Member member = mService.selectMemberById(b.getId());
-                // 產生優惠券代碼(預設為6位數)
-                String couponCode = couponUsage.generateCouponCode(6);
-                // 發送優惠券Email
-                couponUsage.sendMail(member, couponCode);
-                
-                /* ================================== */
-            }
-            System.out.println("================寄送Email完成！==================");
+			StringBuilder sb = new StringBuilder();
+			// u.getProtocol(): "http" or "https"
+			sb.append(u.getProtocol()+"://");
+			// u.getAuthority(): localhost:8080
+			sb.append(u.getAuthority());
+			// request.getContextPath(): /FoodMap04
+			sb.append(request.getContextPath());
+			String baseUrl = sb.toString();
+			
+			/* =====寄信===== */
+			CouponUsageUtil couponUsage = new CouponUsageUtil(baseUrl);
+			System.out.println("================正在寄送Email...==================");
+			for (QuestionnaireBean b : sendUsersCoupons) {
+				System.out.println(b.getId());
+				/* ================================== */
+				/* 待修改成問卷表格與會員表格join的形式 */
+				
+				// 由問卷表格查詢該身分證對應的會員
+				Member member = mService.selectMemberById(b.getId());
+				// 產生優惠券代碼(預設為6位數)
+				String couponCode = couponUsage.generateCouponCode(6);
+				// 發送優惠券Email
+				couponUsage.sendMail(member, couponCode);
+				
+				/* ================================== */
+			}
+			System.out.println("================寄送Email完成！==================");
 			label = "sendCoupons";
 		} else if("B".equals(action)){
 			List<QuestionnaireBean> revokeUsersCoupons = qService.revokeUsersCoupons();
@@ -94,7 +94,5 @@ public class AdminController {
 		return PREFIX+label;
 		
 	}
-	
-	
-	
+
 }
--- src/main/java/cf/cvb14795/member/controller/ForgetPassword.java
+++ src/main/java/cf/cvb14795/member/controller/ForgetPassword.java
@@ -1,10 +1,7 @@
 package cf.cvb14795.member.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Date;
@@ -22,16 +19,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.HttpSessionRequiredException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.azure.core.annotation.QueryParam;
 import com.google.gson.Gson;
 
 import cf.cvb14795.member.bean.Member;
@@ -42,7 +34,6 @@ import util.gmail.Mail;
 @RequestMapping("/Member")
 public class ForgetPassword {
 	private String prefix = "Member/";
-	private String userAccount;
 	
 	@Autowired
 	IMemberService mService;
@@ -75,11 +66,10 @@ public class ForgetPassword {
 
 		System.out.println("正在確認註冊時信箱是否正確...");
 		Member m = mService.selectMemberByAccount(inputAccount);
-		String dbAccount = m.getAccount();
+		String userAccount = m.getAccount();
 		System.out.println("=====ForgetPassword=====");
 		System.out.println("使用者輸入帳號:"+inputAccount);
-		System.out.println("資料庫用戶帳號:"+dbAccount);
-		userAccount = inputAccount;
+		System.out.println("資料庫用戶帳號:"+userAccount);
 		boolean isAccountCorrect = m.getAccount().equals(inputAccount);
 		boolean isEmailCorrect = m.getEmail().equals(recipientEmail);
 		System.out.println("isAccountCorrect: "+isAccountCorrect);
@@ -135,97 +125,4 @@ public class ForgetPassword {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
-	@GetMapping("ResetPassword")
-	private String resetPasswordPage(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			@ModelAttribute("user") String user,
-			@QueryParam("token") String token) throws IOException {
-			
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		System.out.println("=====ResetPassword=====");
-		
-		
-		// 字串不能用== 要用equals
-		if (token != "" && token != null){
-			byte[] bytes = Base64.getDecoder().decode(token);
-			// [0]: user
-			// [1]: token過期時間 (單位:秒)
-			String[] tokenArray = new String(bytes, StandardCharsets.UTF_8).split("-");
-			System.out.println("user(encrypt):"+tokenArray[0]);
-			System.out.println("expire date:"+tokenArray[1]);
-			if (user.equals("")) {
-				user = userAccount;
-			}
-			System.out.println("user(textplain): "+user);
-			boolean isAccountVaild = BCrypt.checkpw(user, tokenArray[0]);
-			// 獲取當前時間 (單位:毫秒)
-			Date now = new Date();
-			Date expiredDate = new Date(Long.valueOf(tokenArray[1])*1000);
-			// 現在時間較過期時間晚 即表示該token已過期
-			boolean isExpired =  now.after(expiredDate);
-			System.out.println("isAccountVaild:"+isAccountVaild);
-			System.out.println("isExpired:"+isExpired);
-			System.out.println("驗證時間:"+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(now));
-			System.out.println("過期時間:"+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(expiredDate));
-			if (isAccountVaild && !isExpired) {
-				System.out.println("Ｏreset: 帳號驗證成功!");
-				return prefix + "resetPassword";
-			} else {
-				System.out.println("＊reset: 帳號驗證失敗!");
-				// token不合法或無user名
-				out.write("<p>驗證失敗！<br/>可能是您已進行過密碼變更，或該Email連結已過期！<p>");
-				return null;
-			}
-		} else {
-			// token不合法或無user名
-			out.write("<p>驗證失敗！<br/>可能是您已進行過密碼變更，或該Email連結已過期！<p>");
-			return null;
-		}
-	}
-	
-	@PostMapping("ResetPassword")
-	@ResponseBody
-	private ResponseEntity<String> doResetPassword(
-			@ModelAttribute("user") String user,  
-			@RequestParam("password") String password,
-			HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-
-		/** Authorizes the installed application to access user's protected data. */
-		String message;
-		HttpHeaders responseHeaders = new HttpHeaders();
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
-
-		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
-		System.out.println("==="+user+"====");
-		Member m = mService.selectMemberByAccount(user);
-		boolean isSuccess = mService.updateMemberPassword(hashedPassword, m);
-		if (isSuccess) {
-			message = "修改成功!!!";
-			System.out.println(message);
-			responseHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-			responseHeaders.add("Content-Type", "application/json; charset=utf-8");
-			return ResponseEntity.ok()
-				      .headers(responseHeaders)
-				      .body(new Gson().toJson(message));		
-		}
-		return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
-	}
-
-	@ExceptionHandler(HttpSessionRequiredException.class)
-	public String ExceptionHandler(
-			HttpSessionRequiredException ex,
-			HttpServletRequest request){
-        String msg = "Error: "+ex.getMessage()+"\n偵測到使用者尚未登入，將初始化user!";
-		System.out.println(msg);
-        // 初始化user
-        request.getSession().setAttribute("user", userAccount);
-        return "redirect:/Member/ResetPassword";
-    }
-
 }
