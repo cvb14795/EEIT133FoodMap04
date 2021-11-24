cbedd175b0bf119c1548e7ffd31f1efd249f1ab8
cvb14795 <cvb14795@gmail.com>
2021-11-24 08:57:20 +0800

    fix shop item 404 bug & update oauth2 login

--- src/main/java/util/Test.java
+++ src/main/java/util/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean checkpw = BCrypt.checkpw("test", "$2a$10$4myoUM6Wa1Nm34KDBP7FbuVKcsqffE3Vo9Q.hTnpqc02AAGAygdsi");
+//		boolean checkpw = BCrypt.checkpw("", "$2a$10$zHKYCbQu4NCHQ2xSmGXKguNakPeCRLnj3X6WzUyq6tbnueFf5lfu6");
 		System.out.println(checkpw);
 	}
 
