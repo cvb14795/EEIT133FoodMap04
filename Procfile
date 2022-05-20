d9830591196644ea33932d8becf0f58297ed334b
cvb14795 <cvb14795@gmail.com>
2022-05-21 00:16:41 +0800

    郵件參數測試

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT --path /FoodMap04 target/*.war
+web: java -Djava.awt.headless=true -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -XX:+UseConcMarkSweepGC -jar target/dependency/webapp-runner.jar --port $PORT --path /FoodMap04 target/*.war
--- src/main/java/util/Test.java
+++ src/main/java/util/Test.java
@@ -2,13 +2,18 @@ package util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import com.microsoft.azure.storage.core.Base64;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean checkpw = BCrypt.checkpw("test", "$2a$10$4myoUM6Wa1Nm34KDBP7FbuVKcsqffE3Vo9Q.hTnpqc02AAGAygdsi");
+//		boolean checkpw = BCrypt.checkpw("test", "$2a$10$4myoUM6Wa1Nm34KDBP7FbuVKcsqffE3Vo9Q.;hTnpqc02AAGAygdsi");
 //		boolean checkpw = BCrypt.checkpw("", "$2a$10$zHKYCbQu4NCHQ2xSmGXKguNakPeCRLnj3X6WzUyq6tbnueFf5lfu6");
-		System.out.println(checkpw);
+//		System.out.println(checkpw);
+		String hashpw = BCrypt.hashpw("eeit133group4", BCrypt.gensalt(10));
+		String result = Base64.encode(String.join("-", hashpw,String.valueOf(System.currentTimeMillis())).getBytes());
+		System.out.println(result);
 	}
 
 }
