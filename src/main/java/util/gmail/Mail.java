637decaa73cc85634d92c95a8213af413ffc687e
cvb14795 <cvb14795@gmail.com>
2022-05-22 01:41:20 +0800

    修正沒加到compoent註釋

--- src/main/java/util/gmail/Mail.java
+++ src/main/java/util/gmail/Mail.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+@Component
 public class Mail {
 	@Autowired
 	private JavaMailSender mailSender;
