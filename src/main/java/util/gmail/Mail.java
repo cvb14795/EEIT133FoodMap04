2ed94a55ddfa0401ec0d2a56c64f27fde20f0e53
cvb14795 <cvb14795@gmail.com>
2022-05-22 01:11:13 +0800

    deploy

--- src/main/java/util/gmail/Mail.java
+++ src/main/java/util/gmail/Mail.java
@@ -1,19 +1,20 @@
 package util.gmail;
 
-import javax.mail.MessagingException;
-import javax.mail.Transport;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public class Mail {
 	@Autowired
 	private JavaMailSender mailSender;
 
+	@Value("${spring.mail.password}")
+	private String pwd;
 	/**
-	 * @param from 寄件者(預設為userAccount)
 	 * @param to 收件者
 	 * @param subject 主旨
 	 * @param text 內文(可包含HTML元素)
@@ -30,6 +31,7 @@ public class Mail {
 			// 傳送含有html標籤的訊息
 			mailSender.send(message);
 			System.out.println("郵件寄送成功! 訊息:"+message);
+			System.out.println(pwd);
 			
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
